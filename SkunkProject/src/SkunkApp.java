b381a1694cfd78fbaf8613b7ca5ea9454aa88a86
evlevel <evlevel@stthomas.edu>
2018-10-12 12:55:02 -0500

    partial SkunkApp Turn implementation, along with Roll stuff

--- SkunkProject/src/SkunkApp.java
+++ SkunkProject/src/SkunkApp.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.introcs.StdIn;
 import edu.princeton.cs.introcs.StdOut;
 
 public class SkunkApp
@@ -6,7 +7,43 @@ public class SkunkApp
 	public static void main(String[] args)
 	{
 		StdOut.println("Welcome to 635 Skunk project!");
+		
+		StdOut.println("Play one Turn for player1");
+		
+		String player = "player1";
+		
+		Turn turn = new Turn(); 
+		
+		while (true)
+		{
+			StdOut.print("Do you wish to roll again? 'n' or 'y' (RETURN): ");
+			String reply = StdIn.readLine();
+			
+			if (reply.equals("n"))
+				break;
+			
+			turn.doRoll();
+			
+			int rollScore = turn.getLastRollScore();
+			
+			if (rollScore == 0) // skunk?
+			{
+				StdOut.println("Roll result: " + rollResult());
+			}
+			
+		}
+		
+		String turnResult = turn.getTurnResult();
+		
+		StdOut.println("Turn result: " + turnResult);
+		
 
+	}
+
+	private static String rollResult()
+	{
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- SkunkProject/src/Turn.java
+++ SkunkProject/src/Turn.java
@@ -1,13 +1,56 @@
+import java.util.ArrayList;
 
 public class Turn
 {
+	
+	private int turnScore;
+	private int lastRollScore;
+	private Roll lastRoll;
+	private ArrayList<Roll> allRolls = new ArrayList();
+	private boolean notOver;
+	private int playerChips;
+	private int kitty;
 
-	public Turn(String playerName)
+	public Turn()
 	{
 		// TODO Auto-generated constructor stub
 	}
 
 	public Object getTurnScore()
+	{
+		// TODO Auto-generated method stub
+		return this.turnScore;
+	}
+	
+	public int getLastRollScore()
+	{
+		return lastRollScore;
+	}
+
+	public boolean notDone()
+	{
+		// TODO Auto-generated method stub
+		return notOver;
+	}
+
+	public void doRoll()
+	{
+		this.lastRoll = new Roll();
+		if (lastRoll.isSingleSkunk())
+		{
+			lastRollScore = 0;
+			
+		}
+			
+	}
+
+	public String getTurnResult()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getRollResult()
 	{
 		// TODO Auto-generated method stub
 		return null;
